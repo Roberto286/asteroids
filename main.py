c0769276207d01d69ec1ce72071d727719bd3b14
Roberto Saliola <roberto.saliola96@gmail.com>
2026-02-19 19:16:45 +0100

    60 fps lmit

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+
     screen.fill("black")
     display.flip()
 
