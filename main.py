5a0c7c6e1b418b7076f9744b684945433acedf29
Roberto Saliola <roberto.saliola96@gmail.com>
2026-02-19 19:39:13 +0100

    created and rendered palyer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,6 +11,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,8 +20,9 @@ def main():
                 return
         dt = clock.tick(60) / 1000
 
-    screen.fill("black")
-    display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
