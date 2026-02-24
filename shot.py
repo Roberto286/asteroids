ab7e8f7330e662723eecf020505ea81cdd551826
Roberto Saliola <roberto.saliola96@gmail.com>
2026-02-24 15:21:32 +0100

    add asteroid kill
    add pyright config file
    
    refactor player class to fix shot position

--- shot.py
+++ shot.py
@@ -1,14 +1,14 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, SHOT_RADIUS
 
 class Shot(CircleShape):
     velocity = 0
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
         self.x = x
         self.y = y
-        self.radius = radius
+        self.radius = SHOT_RADIUS 
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
